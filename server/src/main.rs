51ffb14f7d706e3bd0532d474f6a5c2bbc7a850c
flauntingspade4 <elliotwhybrow@gmail.com>
2021-07-17 21:47:39 +0100

    Revert "Change from actix to salvo"
    This reverts commit b1e7e4f5d3a4f33d747e47957d38f2313c741d51.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,22 +1,3 @@
-#![warn(clippy::nursery, clippy::pedantic)]
-
-mod game;
-
-mod new_game;
-
-use new_game::new_game_handle;
-
-use salvo::{async_trait, fn_handler, Response, Router, Server};
-
-#[fn_handler]
-async fn hello_world(res: &mut Response) {
-    res.render_plain_text("Hello World");
-}
-
-#[tokio::main]
-async fn main() {
-    let router = Router::new()
-        .handle(hello_world)
-        .push(Router::new().path("newGame").handle(new_game_handle));
-    Server::new(router).bind(([0, 0, 0, 0], 7878)).await;
+fn main() {
+    println!("Hello, world!");
 }
