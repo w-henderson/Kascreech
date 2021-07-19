1771dec915c5b90f4004d0596710f062cc323258
flauntingspade4 <elliotwhybrow@gmail.com>
2021-07-19 11:19:19 +0100

    Add some basic logging features

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,40 +1,54 @@
 #![warn(clippy::nursery, clippy::pedantic)]
-#![feature(once_cell)]
 
 mod command;
 mod err;
 mod game;
+mod player;
 
-mod new_game;
+mod host;
+mod join;
 
 use command::Command;
 use game::Game;
-use new_game::host_command;
 
-use dashmap::DashMap;
-
-use futures::{stream::SplitSink, StreamExt};
-
-use once_cell::sync::Lazy;
+use log::{info, warn};
+use simple_log::LogConfigBuilder;
 
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::{tungstenite::Message, WebSocketStream};
 
-type Write = SplitSink<WebSocketStream<TcpStream>, Message>;
+use dashmap::DashMap;
 
-static GAMES: Lazy<DashMap<String, Game>> = Lazy::new(|| DashMap::default());
+use futures::{
+    stream::{SplitSink, SplitStream},
+    StreamExt,
+};
+
+use once_cell::sync::Lazy;
+
+type Write = SplitSink<WebSocketStream<TcpStream>, Message>;
+type Read = SplitStream<WebSocketStream<TcpStream>>;
+
+static GAMES: Lazy<DashMap<String, Game>> = Lazy::new(DashMap::default);
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let log_config = LogConfigBuilder::builder()
+        .path("log.log")
+        .output_file()
+        .build();
+
+    simple_log::new(log_config).unwrap();
+
     let addr = std::env::args()
         .nth(1)
-        .unwrap_or_else(|| "0.0.0.0:8080".to_string());
+        .unwrap_or_else(|| "0.0.0.0:80".to_string());
 
-    // Create the event loop and TCP listener we'll accept connections on.
     let try_socket = TcpListener::bind(&addr).await;
     let listener = try_socket.expect("Failed to bind");
 
     while let Ok((stream, _)) = listener.accept().await {
+        info!("New connection");
         tokio::spawn(accept_connection(stream));
     }
 
@@ -52,10 +66,10 @@ async fn accept_connection(stream: TcpStream) {
         if let Ok(s) = message.to_text() {
             if let Ok(command) = serde_json::from_str::<Command>(s) {
                 match command.command {
-                    "host" => host_command(s, &mut write).await,
+                    "host" => host::host_command(s, &mut write, &mut read).await,
+                    "join" => join::join_command(s, &mut write).await,
                     _ => {
-                        eprintln!("Unknown command type {}", command.command);
-                        todo!("Error handling for incorrect command type");
+                        warn!("Unknown command type {}", command.command);
                     }
                 }
             }
