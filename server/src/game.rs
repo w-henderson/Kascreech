40a511774ef793aacbb0d2129f953c119ace1b8b
flauntingspade4 <elliotwhybrow@gmail.com>
2021-07-19 11:22:24 +0100

    Add a `Player` type Update the `Game`, to allow new players to notify the `Game` when they've joined, so they can be displayed on the big screen

--- server/src/game.rs
+++ server/src/game.rs
@@ -1,30 +1,56 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
-use crate::err::KascreechError;
+use tokio::sync::mpsc::Sender;
 
-use ureq::get;
+use crate::{err::KascreechError, player::Player};
 
-#[derive(Debug, Serialize, Deserialize)]
+use ureq::{get, Error};
+
 pub struct Game {
     pub title: String,
-    pub questions: Vec<Question>,
+    pub questions: Vec<KahootQuestion>,
+    pub players: HashMap<String, Player>,
+    pub host_sender: Sender<String>,
 }
 
 impl Game {
-    pub fn from_url(path: &str) -> Result<Self, KascreechError> {
-        get(path).call()?.into_json().map_err(Into::into)
+    pub fn new(url: &str, host_sender: Sender<String>) -> Result<Self, KascreechError> {
+        match get(url).call() {
+            Ok(res) => {
+                let kahoot_game: KahootGame = res.into_json()?;
+
+                Ok(Self {
+                    title: kahoot_game.title,
+                    questions: kahoot_game.questions,
+                    players: HashMap::new(),
+                    host_sender,
+                })
+            }
+            Err(err) => match err {
+                Error::Status(e, _) if e == 404 => Err(KascreechError::GameNotFound),
+                _ => Err(err.into()),
+            },
+        }
     }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Question {
-    question: String,
-    time: usize,
-    choices: Vec<Answer>,
+struct KahootGame {
+    title: String,
+    questions: Vec<KahootQuestion>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Answer {
+pub struct KahootQuestion {
+    question: String,
+    time: usize,
+    choices: Vec<KahootAnswer>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KahootAnswer {
     answer: String,
     correct: bool,
 }
---
+++ server/src/player.rs
@@ -0,0 +1,4 @@
+#[derive(Default)]
+pub struct Player {
+    pub score: usize,
+}
