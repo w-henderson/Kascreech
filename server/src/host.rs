4a28819fc896da241ae6a87b319156ad6a6ee768
flauntingspade4 <elliotwhybrow@gmail.com>
2021-08-06 20:20:22 +0100

    Update hosting

--- server/src/host.rs
+++ server/src/host.rs
@@ -7,17 +7,44 @@ use tokio_tungstenite::tungstenite::Message;
 
 use crate::{
     command::Command,
+    err::{KascreechError, KascreechResult},
     game::{Game, KahootAnswer},
     player::Player,
-    Read, Write, GAMES,
+    Read, Senders, Write, GAMES, HOST_SENDERS,
 };
 
 use log::warn;
 
-use futures::{select, FutureExt, SinkExt, StreamExt};
+use futures::{select, SinkExt, StreamExt};
 
-pub async fn host_command(host_request: &str, write: &mut Write, read: &mut Read) {
-    let host_request = serde_json::from_str::<HostRequest>(host_request).unwrap();
+pub async fn host_command(
+    host_request: &str,
+    write: &mut Write,
+    read: &mut Read,
+) -> KascreechResult<()> {
+    let game_id = generate_game_id().await;
+
+    let to_return = host_command_internal(game_id.clone(), host_request, write, read).await;
+
+    GAMES.remove(&game_id);
+    HOST_SENDERS.remove(&game_id);
+
+    to_return
+}
+
+async fn generate_game_id() -> String {
+    repeat_with(|| fastrand::digit(10)).take(6).collect()
+}
+
+async fn host_command_internal(
+    game_id: String,
+    host_request: &str,
+    write: &mut Write,
+    read: &mut Read,
+) -> KascreechResult<()> {
+    let mut read = read.fuse();
+
+    let host_request = serde_json::from_str::<HostRequest>(host_request)?;
 
     let (player_sender, player_receiver) = tokio::sync::mpsc::channel(5);
     let mut player_receiver = ReceiverStream::new(player_receiver).fuse();
@@ -25,27 +52,18 @@ pub async fn host_command(host_request: &str, write: &mut Write, read: &mut Read
     let (question_sender, question_receiver) = tokio::sync::mpsc::channel(5);
     let mut question_receiver = ReceiverStream::new(question_receiver).fuse();
 
-    let game_id: String = repeat_with(|| fastrand::digit(10)).take(6).collect();
-
     // Creating a new game
-    match Game::new(
+    let game = Game::new(
         &format!("https://play.kahoot.it/rest/kahoots/{}", host_request.id),
         player_sender,
-        question_sender,
-    ) {
-        Ok(game) => {
-            let message =
-                Message::Text(serde_json::to_string(&game.make_response(&game_id)).unwrap());
-            write.send(message).await.unwrap();
+    )?;
 
-            GAMES.insert(game_id.clone(), game);
-        }
-        Err(e) => {
-            crate::send_error!(write, e).unwrap();
+    let message = Message::Text(serde_json::to_string(&game.make_response(&game_id))?);
+    write.send(message).await?;
 
-            return;
-        }
-    }
+    GAMES.insert(game_id.clone(), game);
+
+    HOST_SENDERS.insert(game_id.clone(), Senders::new(question_sender));
 
     // Wait for either new players to join,
     // or the server to start the game
@@ -56,20 +74,20 @@ pub async fn host_command(host_request: &str, write: &mut Write, read: &mut Read
                 if let Some(new_player) = recv {
                     let message = Message::Text(serde_json::to_string(&PlayerJoined {
                         event: "newPlayer", player_name: &new_player
-                    }).unwrap());
+                    })?);
 
-                    write.send(message).await.unwrap();
+                    write.send(message).await?;
                 }
             },
             // The host's began the game
-            message = read.next().fuse() => {
+            message = read.next() => {
                 if let Some(Ok(message)) = message {
                     if let Ok(s) = message.to_text() {
                         if let Ok(request) = serde_json::from_str::<Command>(s) {
                             if request.command == "start" {
                                 break
                             }
-                            warn!("unrecognised command \"{}\" when it should only be \"start\"", request.command);
+                            warn!("Unrecognised command \"{}\" when it should only be \"start\"", request.command);
                         }
                     }
                 }
@@ -81,152 +99,149 @@ pub async fn host_command(host_request: &str, write: &mut Write, read: &mut Read
 
     // The actual game loop
     loop {
-        match game.questions.next() {
-            // Handle the next question
-            Some(next_question) => {
-                let player_send_question = Message::Text(
-                    serde_json::to_string(&PlayerSendQuestion {
-                        event: "questionStart",
-                        number_of_answers: next_question.choices.len(),
-                    })
-                    .unwrap(),
-                );
+        // Handle the next question
+        if let Some(next_question) = game.questions.next() {
+            let player_send_question = Message::Text(serde_json::to_string(&PlayerSendQuestion {
+                event: "questionStart",
+                number_of_answers: next_question.choices.len(),
+            })?);
 
-                for player in &game.players {
-                    player
-                        .player_sender
-                        .send(player_send_question.clone())
-                        .await
-                        .unwrap();
-                }
+            for player in &game.players {
+                player
+                    .player_sender
+                    .send(player_send_question.clone())
+                    .await
+                    .unwrap();
+            }
 
-                let host_send_question = Message::Text(
-                    serde_json::to_string(&HostSendQuestion {
-                        question: &next_question.question,
-                        duration: next_question.time,
-                        answers: &next_question.choices,
-                    })
-                    .unwrap(),
-                );
+            let host_send_question = Message::Text(serde_json::to_string(&HostSendQuestion {
+                question: &next_question.question,
+                duration: next_question.time,
+                answers: &next_question.choices,
+            })?);
 
-                write.send(host_send_question).await.unwrap();
+            write.send(host_send_question).await?;
 
-                game.receiving = true;
+            let mut points = Points::new(game.players.len().try_into().unwrap());
 
-                let mut points = Points::new(game.players.len().try_into().unwrap());
+            // The host is now ready to accept guesses
+            HOST_SENDERS.get_mut(&game_id).unwrap().receiving = true;
 
-                // Waiting for players to send answers, or the host
-                // to request the leaderboard
-                loop {
-                    select! {
-                    // A player's sent an answer
-                    recv = question_receiver.next() => {
-                        if let Some(player_guess) = recv {
-                            let player = game.players.iter_mut().find(|p| p.user_name == player_guess.user_name);
-                            if let Some(player) = player {
-                                // The player has not played this round yet
-                                if !player.played {
-                                    if let Some(choice) = next_question.choices.get(player_guess.index) {
-                                        let (points_this_round, streak) = if choice.correct {
-                                            let add_points = points.next_points();
+            // Waiting for players to send answers, or the host
+            // to request the leaderboard
+            loop {
+                select! {
+                // A player's sent an answer
+                recv = question_receiver.next() => {
+                    if let Some(player_guess) = recv {
+                        // Checking the player exists
+                        if let Some(player) = game.players.iter_mut().find(|p| p.user_name == player_guess.user_name) {
+                            // The player has not played this round yet
+                            if !player.played {
+                                // Checking the guess is valid
+                                if let Some(choice) = next_question.choices.get(player_guess.index) {
+                                    let (points_this_round, streak) = if choice.correct {
+                                        let add_points = points.next_points();
 
-                                            let points_this_round = <u16 as Into<usize>>::into(add_points);
+                                        let points_this_round = <u16 as Into<usize>>::into(add_points);
 
-                                            player.score += points_this_round;
-                                            player.streak += 1;
+                                        player.points += points_this_round;
+                                        player.streak += 1;
 
-                                            (points_this_round, player.streak)
-                                        } else {
-                                            player.streak = 0;
-                                            (0, 0)
-                                        };
+                                        (points_this_round, player.streak)
+                                    } else {
+                                        player.streak = 0;
+                                        (0, 0)
+                                    };
 
-                                        let message = Message::Text(
-                                            serde_json::to_string(&PlayerRoundEnd {
-                                                event: "questionEnd",
-                                                correct: choice.correct,
-                                                points_this_round,
-                                                points_total: player.score,
-                                                streak,
-                                                position: 0,
-                                                behind: None,
-                                            }).unwrap()
-                                        );
-
-                                        player.player_sender.send(message).await.unwrap();
-                                    }
-                                }
-                                player.played = true;
-                            }
-                        }
-                    },
-                    // The host's requested a leaderboard
-                    message = read.next().fuse() => {
-                        if let Some(Ok(message)) = message {
-                                if let Ok(message) = message.to_text() {
-                                    if let Ok(host_request_leaderboard) =
-                                        serde_json::from_str::<Command>(message)
-                                    {
-                                        if host_request_leaderboard.command == "leaderboard" {
-                                            break;
+                                    player.player_round_end = Some(
+                                        PlayerRoundEnd {
+                                            event: "questionEnd",
+                                            correct: choice.correct,
+                                            points_this_round,
+                                            points_total: player.points,
+                                            streak,
+                                            position: 0,
+                                            behind: None,
                                         }
+                                    );
+                                }
+                            }
+                            player.played = true;
+                        }
+                    }
+                },
+                // The host's requested a leaderboard
+                message = read.next() => {
+                    if let Some(Ok(message)) = message {
+                            if let Ok(message) = message.to_text() {
+                                if let Ok(host_request_leaderboard) =
+                                    serde_json::from_str::<Command>(message)
+                                {
+                                    if host_request_leaderboard.command == "leaderboard" {
+                                        break;
                                     }
                                 }
                             }
                         }
                     }
                 }
+            }
 
-                game.receiving = false;
+            HOST_SENDERS
+                .get_mut(&game_id)
+                .ok_or(KascreechError::GameNotFound)?
+                .receiving = false;
 
-                // Actually send the leaderboard
-                game.players.sort_by(|a, b| a.score.cmp(&b.score));
+            // Actually send the leaderboard
+            game.players.sort_by(|a, b| a.points.cmp(&b.points));
 
-                let leader_board_response = Message::Text(
-                    serde_json::to_string(&LeaderBoardResponse {
-                        leaderboard: &game.players,
-                    })
-                    .unwrap(),
-                );
+            let leader_board_response =
+                Message::Text(serde_json::to_string(&LeaderBoardResponse {
+                    leaderboard: &game.players,
+                })?);
 
-                write.send(leader_board_response).await.unwrap();
+            {
+                let player_len = game.players.len();
+                let mut player_peek = game.players.iter_mut().enumerate().peekable();
+                while let Some((pos, player)) = player_peek.next() {
+                    let behind = player_peek.peek().map(|p| p.1.user_name.clone());
+                    player.send(player_len - pos, behind).await.unwrap();
+                }
+            }
 
-                // Waiting for the host to request the next question
-                loop {
-                    if let Some(Ok(message)) = read.next().await {
-                        if let Ok(message) = message.to_text() {
-                            if let Ok(host_request_next_question) =
-                                serde_json::from_str::<Command>(message)
-                            {
-                                if host_request_next_question.command == "question" {
-                                    break;
-                                }
+            write.send(leader_board_response).await?;
+
+            // Waiting for the host to request the next question
+            loop {
+                if let Some(Ok(message)) = read.next().await {
+                    if let Ok(message) = message.to_text() {
+                        if let Ok(host_request_next_question) =
+                            serde_json::from_str::<Command>(message)
+                        {
+                            if host_request_next_question.command == "question" {
+                                break;
                             }
                         }
                     }
                 }
             }
-            // No questions remain, the game ends
-            None => {
-                game.players.sort_by(|a, b| a.score.cmp(&b.score));
-                for (i, player) in game.players.iter().enumerate() {
-                    let game_over = Message::Text(
-                        serde_json::to_string(&GameOver {
-                            event: "end",
-                            position: i + 1,
-                        })
-                        .unwrap(),
-                    );
-                    player.player_sender.send(game_over).await.unwrap();
-                }
-                break;
+        }
+        // No questions remain, the game ends
+        else {
+            game.players.sort_by(|a, b| a.points.cmp(&b.points));
+            for (i, player) in game.players.iter().enumerate() {
+                let game_over = Message::Text(serde_json::to_string(&GameOver {
+                    event: "end",
+                    position: i + 1,
+                })?);
+                player.player_sender.send(game_over).await.unwrap();
             }
+            break;
         }
     }
 
-    println!("Finished a connection");
-
-    GAMES.remove(&game_id).unwrap();
+    Ok(())
 }
 
 struct Points {
@@ -242,7 +257,7 @@ impl Points {
         }
     }
     fn next_points(&mut self) -> u16 {
-        let power = -(self.placement - 1) / (self.num_of_players - 1);
+        let power = -(self.placement - 1) / (self.num_of_players - 1).max(1);
 
         self.placement += 1;
 
@@ -250,17 +265,17 @@ impl Points {
     }
 }
 
-#[derive(Serialize)]
-struct PlayerRoundEnd {
-    event: &'static str,
-    correct: bool,
+#[derive(Debug, Serialize)]
+pub struct PlayerRoundEnd {
+    pub event: &'static str,
+    pub correct: bool,
     #[serde(rename = "pointsThisRound")]
-    points_this_round: usize,
+    pub points_this_round: usize,
     #[serde(rename = "pointsTotal")]
-    points_total: usize,
-    streak: usize,
-    position: usize,
-    behind: Option<Arc<String>>,
+    pub points_total: usize,
+    pub streak: usize,
+    pub position: usize,
+    pub behind: Option<Arc<String>>,
 }
 
 #[derive(Deserialize)]
@@ -298,8 +313,8 @@ struct PlayerJoined<'a> {
 }
 
 #[derive(Serialize)]
-struct PlayerSendQuestion<'a> {
-    event: &'a str,
+struct PlayerSendQuestion {
+    event: &'static str,
     #[serde(rename = "numberOfAnswers")]
     number_of_answers: usize,
 }
@@ -317,7 +332,7 @@ struct LeaderBoardResponse<'a> {
 }
 
 #[derive(Serialize)]
-struct GameOver<'a> {
-    event: &'a str,
+struct GameOver {
+    event: &'static str,
     position: usize,
 }
