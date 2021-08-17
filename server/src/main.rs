948b59dc4f0e743b5246538514c8b2c6edbe28ab
flauntingspade4 <elliotwhybrow@gmail.com>
2021-08-17 18:27:39 +0100

    Revert broken commit, fixed separately
    This reverts commit 52fcbcf01b0b580bd8a30da4305f2ab918ca0303.

--- server/src/main.rs
+++ server/src/main.rs
@@ -14,15 +14,9 @@ use game::{Game, Senders};
 
 use simple_log::LogConfigBuilder;
 
-use tokio::{
-    io::{self, AsyncReadExt, AsyncWriteExt, BufReader},
-    net::{TcpListener, TcpStream},
-};
+use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::{
-    tungstenite::{
-        protocol::{Role, WebSocketConfig},
-        Message,
-    },
+    tungstenite::{protocol::WebSocketConfig, Message},
     WebSocketStream,
 };
 
@@ -78,91 +72,38 @@ async fn accept_connection(stream: TcpStream) {
     }
 }
 
-async fn accept_connection_internal(mut stream: TcpStream) -> KascreechResult<()> {
-    let mut buf = [0; 1000];
-    let mut buf_reader = BufReader::new(&mut stream);
+async fn accept_connection_internal(stream: TcpStream) -> KascreechResult<()> {
+    let ws_stream =
+        tokio_tungstenite::accept_async_with_config(stream, Some(WEBSOCKET_CONFIG)).await?;
 
-    let read = buf_reader.read(&mut buf).await.unwrap();
+    let (mut write, mut read) = ws_stream.split();
 
-    let mut headers = [httparse::EMPTY_HEADER; 32];
-    let mut req = httparse::Request::new(&mut headers);
+    let message = read
+        .next()
+        .await
+        .ok_or(FailResponse::new(KascreechError::FailedRead, None))??;
 
-    req.parse(&buf).unwrap();
+    let s = message.to_text()?;
 
-    // If the stream is a websocket stream
-    if let Some(header) = req.headers.iter().find(|h| h.name == "Sec-WebSocket-Key") {
-        let mut key = std::str::from_utf8(header.value).unwrap().to_string();
-        key.push_str("258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
-        let sha = sha1::Sha1::from(key);
+    let command = serde_json::from_str::<Command>(s)?;
 
-        let hash = sha.digest().bytes();
-        let key = base64::encode(hash);
+    let err = match command.command {
+        "host" => host::host_command(s, &mut write, &mut read).await,
+        "join" => join::join_command(s, &mut write, &mut read).await,
+        _ => Err(FailResponse::new(
+            KascreechError::UnrecognisedCommand,
+            Some(command.command.to_string()),
+        )),
+    };
 
-        let response = format!(
-                "HTTP/1.1 101 Switching Protocols\r\nConnection: Upgrade\r\nUpgrade: websocket\r\nSec-WebSocket-Accept: {}\r\n\r\n",
-                key
-            );
+    if let Err(e) = err {
+        log::error!("{}", e);
 
-        stream.write(response.as_bytes()).await.unwrap();
-
-        stream.flush().await.unwrap();
-
-        let ws_stream =
-            WebSocketStream::from_raw_socket(stream, Role::Server, Some(WEBSOCKET_CONFIG)).await;
-
-        let (mut write, mut read) = ws_stream.split();
-
-        let message = read
-            .next()
+        write
+            .send(Message::Text(serde_json::to_string(&e).unwrap()))
             .await
-            .ok_or(FailResponse::new(KascreechError::FailedRead, None))??;
-
-        let s = message.to_text()?;
-
-        let command = serde_json::from_str::<Command>(s)?;
-
-        let err = match command.command {
-            "host" => host::host_command(s, &mut write, &mut read).await,
-            "join" => join::join_command(s, &mut write, &mut read).await,
-            _ => Err(FailResponse::new(
-                KascreechError::UnrecognisedCommand,
-                Some(command.command.to_string()),
-            )),
-        };
-
-        if let Err(e) = err {
-            log::error!("{}", e);
-
-            write
-                .send(Message::Text(serde_json::to_string(&e).unwrap()))
-                .await
-                .unwrap();
-        }
-        // Proxy the stream to localhost:8000
-    } else {
-        proxy(stream, &buf[..read]).await;
+            .unwrap();
     }
 
     Ok(())
-}
-
-async fn proxy(mut inbound: TcpStream, already_read: &[u8]) {
-    let mut outbound = TcpStream::connect("localhost:8000").await.unwrap();
-
-    let (mut ri, mut wi) = inbound.split();
-    let (mut ro, mut wo) = outbound.split();
-
-    wo.write_all(already_read).await.unwrap();
-
-    let client_to_server = async {
-        io::copy(&mut ri, &mut wo).await?;
-        wo.shutdown().await
-    };
-
-    let server_to_client = async {
-        io::copy(&mut ro, &mut wi).await?;
-        wi.shutdown().await
-    };
-
-    tokio::try_join!(client_to_server, server_to_client).unwrap();
 }
