0ce5737bb3ce9766b743fbc09722dde9a55ad42d
William Henderson <william-henderson@outlook.com>
2022-04-21 10:37:43 +0100

    Added basic API documentation for reference

--- server/src/api/mod.rs
+++ server/src/api/mod.rs
@@ -1,3 +1,17 @@
+//! # API Documentation
+//!
+//! ## `/api/v1/import`
+//! Takes a JSON object with the `id` field and imports it from Kahoot into the database. Returns the ID of the imported game in the `gameId` field.
+//!
+//! ## `/api/v1/featured`
+//! Takes no arguments and returns featured games as an array.
+//!
+//! ## `/api/v1/list`
+//! Takes a JSON object with the `offset` field and lists 10 games starting at that offset. Returns an array of game objects.
+//!
+//! ## `/api/v1/search`
+//! Takes a JSON object with the `query` field and searches for games with that query. Returns an array of game objects.
+
 mod featured;
 mod import;
 mod list;
